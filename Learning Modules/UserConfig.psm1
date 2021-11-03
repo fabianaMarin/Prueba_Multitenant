f3c95e22b362323614da78e5ee75b9ecb49f3c8c
fabianaMarin <93681907+fabianaMarin@users.noreply.github.com>
2021-11-03 15:41:08 -0500

    Update UserConfig.psm1
    se cambia grupo de recursos y nombre de usuario

--- Learning Modules/UserConfig.psm1
+++ Learning Modules/UserConfig.psm1
@@ -7,8 +7,8 @@
 
 ## Update the two variables below to the values used when the Wingtip SaaS app was deployed.   
 
-$ResourceGroupName = "<resourcegroup>" # the resource group used when the Wingtip SaaS application was deployed. CASE SENSITIVE
-$User              = "<user>"         # the User value entered when the Wingtip SaaS application was deployed
+$ResourceGroupName = "<Prueba_Multitenant>" # the resource group used when the Wingtip SaaS application was deployed. CASE SENSITIVE
+$User              = "<fm1>"         # the User value entered when the Wingtip SaaS application was deployed
 
 ##  DO NOT CHANGE VALUES BELOW HERE -------------------------------------------------------------
 
